5298ff86e2b873f10db32d982a7c482444b07619
m4rc1e <m.foley.88@gmail.com>
2015-09-12 23:59:26 +0200

    process Hindi, Nepali and Marathi conjuncts

--- conjuncts.py
+++ conjuncts.py
@@ -1,27 +1,32 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-import sys
+import os
 from lxml import etree
 import re
 from collections import Counter
 
 def main():
-    xml_file = "./sourcedata/hiwiki-20150901-pages-meta-current.xml"
+    corpus_folder = "./sourcedata/"
+
     count = Counter()
     devaConjuncts = ur"([^\u0930\W]\u094d.\u094d.\u094d[^\u0200\W]|[^\u0930\W]\u094d.\u094d[^\u0200\W]|[^\u0930\W]\u094d[^\u0200\W])"
 
-    a = 0
-    for event, element in etree.iterparse(xml_file):
-        
-        if 'text' in element.tag and element.text != None:
-            texter = (element.text)
-            conjuncts = re.findall(devaConjuncts, texter, re.UNICODE)
-            for conjunct in conjuncts:
-            	count[conjunct] += 1
+    for xml in os.listdir(corpus_folder):
+        if not xml.startswith('.'):
+            print("Parsing %s" %xml)
+            for event, element in etree.iterparse(corpus_folder+xml):
+                if 'text' in element.tag and element.text != None:
+                    texter = (element.text)
+                    conjuncts = re.findall(devaConjuncts, texter, re.UNICODE)
+                    for conjunct in conjuncts:
+                    	count[conjunct] += 1
 
-    for i in sorted(count, key=count.get)[::-1]:
-    	print(count[i], i.encode("utf-8"))
+    conjuncts_file = open("./conjuncts_all.txt", "w")
+    for conjunct in sorted(count, key=count.get)[::-1]:
+    	conjuncts_file.write("%s %s\n" %(count[conjunct], conjunct.encode("utf-8")))
+    conjuncts_file.close()
+    print("Done!")
 
 
 if __name__ == "__main__":
